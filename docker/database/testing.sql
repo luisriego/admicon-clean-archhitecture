a96d554dd46a6c61720c38adcf289d9496a3016c
Jose Luis Riego Montero <luisriego@users.noreply.github.com>
2022-12-20 19:17:47 -0300

    Update testing.sql

--- docker/database/testing.sql
+++ docker/database/testing.sql
@@ -1,1 +1,1 @@
-CREATE DATABASE IF NOT EXISTS database_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS mysql_symfony_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
