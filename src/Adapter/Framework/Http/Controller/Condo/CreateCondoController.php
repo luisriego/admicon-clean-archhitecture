c28f9fd07000cb2b590a211bd06eb4ea24fa04af
luisriego <luisriego@hotmail.com>
2022-12-22 16:21:09 -0300

    Model and Repo creates, migrations ok, migrations on test environment fail w/ controller

---
+++ src/Adapter/Framework/Http/Controller/Condo/CreateCondoController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Adapter\Framework\Http\Controller\Condo;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CreateCondoController extends AbstractController
+{
+
+}
